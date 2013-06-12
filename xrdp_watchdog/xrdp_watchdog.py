c6259a0b2217e57a694a899935e69210eedf2096
asafge <speidy@gmail.com>
2013-06-12 12:45:13 -0700

    Added rm function and some refactoring

--- xrdp_watchdog/xrdp_watchdog.py
+++ xrdp_watchdog/xrdp_watchdog.py
@@ -1,48 +1,60 @@
 import subprocess
 import socket
+import glob
+import os
 
 import retry
 
 
 def is_process(name):
+    print "Check process: %s \t" % name,
     ps = subprocess.Popen("ps -ef | grep %s | grep -v grep" %name, shell=True, stdout=subprocess.PIPE)
     out = ps.stdout.read()
     ps.stdout.close()
     ps.wait()
-    return True if out else False
+    if out:
+        print "[OK]"
+        return True
+    else:
+        print "[ERR]"
+        return False
 
 
-def kill_Process(name):
+def kill_process(name):
+    print "Killing %s" %name
     ps = subprocess.Popen("pkill %s" % name, shell=True)
     ps.wait()
-    return
+
+
+def rm_files(pattern):
+    files_iter = glob.iglob(pattern)
+    for f in files_iter:
+        os.remove(f)
 
 
 def is_tcp_listen(host, port):
     s = socket.socket()
     try:
+        print "Check connection: \t",
         s.connect((host, port))
+        print "[OK]"
         return True
     except:
+        print "[ERR]"
         return False
 
 
-@retry(Exception, tries=3, delay=3, backoff=2)
+#@retry(Exception, tries=3, delay=3, backoff=2)
 def check_xrdp(path, host, port):
-    print "Check process: %s \t" % path,
     if is_process(path):
-        print "[OK]"
-        print "Check connection: \t"
-        if is_tcp_listen(host, port):
-            print "[OK]"
-        else:
-            print "[ERR]"
-            print "Killing %s" %path
+        if not is_tcp_listen(host, port):
             kill_process(path)
+            rm_files("/var/run/xrdp.pid")
+            #rm_files("/var/tmp/.xrdp/xrdp-[^sescv")
     else:
-        print "[ERR]"
-        raise Exception()
+        return 
 
 
 if __name__ == "__main__":
-    check_xrdp("xrdp", "0.0.0.0", "3389")
+    check_xrdp("/usr/local/sbin/xrdp", "0.0.0.0", "3389")
+
